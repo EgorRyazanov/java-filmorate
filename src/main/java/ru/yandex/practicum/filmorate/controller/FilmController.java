c8308e8ab1a847c6d9ff14e23f6b9287dd32a0f7
e.ryazanov <e.ryazanov@tbank.ru>
2025-09-21 20:54:38 +0300

    feat: добавить UserController и FilmController

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,71 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.errors.NotFoundException;
+import ru.yandex.practicum.filmorate.errors.ValidationError;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.utils.ErrorText;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
+@RequestMapping("/films")
 public class FilmController {
+    private final static Logger log = LoggerFactory.getLogger(FilmController.class);
+    private final Map<Integer, Film> films = new HashMap<>();
+
+    @GetMapping
+    public Collection<Film> findAll() {
+        log.info("Получен запрос на получение всех фильмов");
+        return films.values();
+    }
+
+    @PostMapping
+    public Film create(@RequestBody Film film) {
+        log.info("Получен запрос на создание фильма");
+        if (!isFilmValid(film)) {
+            log.error("Произошла ошибка валидации");
+            throw new ValidationError(ErrorText.validationError);
+        }
+        film.setId(getNextId());
+        films.put(film.getId(), film);
+        log.info("Запрос на создание фильма завершился успешно");
+        return film;
+    }
+
+    @PutMapping
+    public Film update(@RequestBody Film film) {
+        log.info("Получен запрос на обновление фильма");
+
+        if (!films.containsKey(film.getId())) {
+            log.error("Объект не был найден");
+            throw new NotFoundException(ErrorText.notFoundError);
+        }
+
+        if (!isFilmValid(film)) {
+            log.error("Произошла ошибка валидации");
+            throw new ValidationError(ErrorText.validationError);
+        }
+        films.put(film.getId(), film);
+        log.info("Запрос на обновление фильма завершился успешно");
+        return film;
+    }
+
+    private Boolean isFilmValid(Film film) {
+        return !film.getName().isEmpty() && film.getDescription().length() <= 200 && film.getReleaseDate().isAfter(LocalDate.of(1895, 12, 28)) && film.getDuration() > 0;
+    }
+
+
+    private Integer getNextId() {
+        int currentMaxId = films.keySet()
+                .stream()
+                .max(Integer::compare)
+                .orElse(0);
+        return ++currentMaxId;
+    }
 }
---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -0,0 +1,78 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.errors.NotFoundException;
+import ru.yandex.practicum.filmorate.errors.ValidationError;
+import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.utils.ErrorText;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final static Logger log = LoggerFactory.getLogger(UserController.class);
+    private final Map<Integer, User> users = new HashMap<>();
+
+    @GetMapping
+    public Collection<User> findAll() {
+        log.info("Получен запрос на получение всех пользователей");
+        return users.values();
+    }
+
+    @PostMapping
+    public User create(@RequestBody User user) {
+        log.info("Получен запрос на создание пользователя");
+        if (!isUserValid(user)) {
+            log.error("Произошла ошибка валидации");
+            throw new ValidationError(ErrorText.validationError);
+        }
+        user.setId(getNextId());
+        if (user.getName() == null) {
+            log.trace("Имя пользователя пустое, будет использоваться логин");
+            user.setName(user.getLogin());
+        }
+        users.put(user.getId(), user);
+        log.info("Запрос на создание пользователя завершился успешно");
+        return user;
+    }
+
+    @PutMapping
+    public User update(@RequestBody User user) {
+        log.info("Получен запрос на обновление пользователя");
+        if (!users.containsKey(user.getId())) {
+            log.error("Объект не был найден");
+            throw new NotFoundException(ErrorText.notFoundError);
+        }
+
+        if (!isUserValid(user)) {
+            log.error("Произошла ошибка валидации");
+            throw new ValidationError(ErrorText.validationError);
+        }
+        if (user.getName() == null) {
+            log.trace("Имя пользователя пустое, будет использоваться логин");
+            user.setName(user.getLogin());
+        }
+
+        users.put(user.getId(), user);
+        log.info("Запрос на обновление пользователя завершился успешно");
+        return user;
+    }
+
+    private Boolean isUserValid(User user) {
+        return !user.getEmail().isEmpty() && user.getEmail().contains("@") && !user.getLogin().isEmpty() && !user.getLogin().contains(" ") && user.getBirthday().isBefore(LocalDate.now());
+    }
+
+    private Integer getNextId() {
+        int currentMaxId = users.keySet()
+                .stream()
+                .max(Integer::compare)
+                .orElse(0);
+        return ++currentMaxId;
+    }
+}

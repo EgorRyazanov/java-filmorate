c9cbd0df7177b19868756d98548376e04f5f4a30
e.ryazanov <e.ryazanov@tbank.ru>
2025-09-28 03:07:42 +0300

    chore: исправить модели storage

--- src/main/java/ru/yandex/practicum/filmorate/model/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/FilmStorage.java
@@ -4,11 +4,18 @@ import java.util.List;
 
 public interface FilmStorage {
     List<Film> getAllFilms();
+
     Film createFilm(Film film);
+
     Film updateFilm(Film film);
+
     boolean containsFilm(Integer id);
+
     Film getFilmById(Integer id);
+
     void addLike(Integer id, Integer userId);
+
     void deleteLike(Integer id, Integer userId);
+
     List<Film> getPopularFilms(Integer count);
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/UserStorage.java
@@ -6,11 +6,18 @@ import java.util.Set;
 
 public interface UserStorage {
     List<User> getAllUsers();
+
     User createUser(User user);
+
     User updateUser(User user);
+
     boolean containsUser(Integer id);
+
     User getUserById(Integer id);
+
     void addFriend(Integer id, Integer friendId);
+
     void deleteFriend(Integer id, Integer friendId);
+
     Set<Integer> getFriends(Integer id);
 }

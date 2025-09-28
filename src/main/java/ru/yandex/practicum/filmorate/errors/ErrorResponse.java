5f36d37bf715851e3b6b61456c8d5ee99b249202
e.ryazanov <e.ryazanov@tbank.ru>
2025-09-28 23:55:55 +0300

    chore: обновить модель ErrorResponse

--- src/main/java/ru/yandex/practicum/filmorate/errors/ErrorResponse.java
+++ src/main/java/ru/yandex/practicum/filmorate/errors/ErrorResponse.java
@@ -3,5 +3,10 @@ package ru.yandex.practicum.filmorate.errors;
 import lombok.Getter;
 
 @Getter
-public record ErrorResponse(String error) {
+public class ErrorResponse {
+    private final String error;
+
+    public ErrorResponse(String error) {
+        this.error = error;
+    }
 }
